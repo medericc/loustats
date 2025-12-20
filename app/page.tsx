c126e92fde7949a2a1f14106a89d53a9c7f66ba7
medericc <bleublanctour1@>
2025-12-20 13:34:51 +0100

    ee

--- app/page.tsx
+++ app/page.tsx
@@ -44,6 +44,8 @@ export default function Home() {
     const [isWaitingModalOpen, setIsWaitingModalOpen] = useState(false);
 
    const matchLinks = [
+     { name: "Lady Rebels", url: "/api/espn?gameId=401820504" } ,
+
     { name: "Lady Rebels", url: "/api/espn?gameId=401825077" } ,
 
 { name: "Sam Houston", url: "/api/espn?gameId=401825076" } ,
